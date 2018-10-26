6f2a61418fb2c739794b05b05391846833a5ef5c
Unknown <nkc.ryota.hirano@gmail.com>
2018-10-26 14:10:39 +0900

    TestControler作ったよ

---
+++ Assets/Hirano/Scripts/PlayerAnimationTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationTest : MonoBehaviour
+{
+    private Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetTrigger("Dash");
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            anim.SetTrigger("Catch");
+        }
+	}
+}
